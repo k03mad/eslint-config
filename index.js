0732ef7ead2da395cde9790a3ec04b67b4f6eb79
k03mad <k03.mad@gmail.com>
2017-11-27 17:04:14 +0300

    remove prefer async-callback

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ module.exports = {
         'promise/catch-or-return': 'error',
         'promise/no-return-wrap': 'error',
         'promise/param-names': 'error',
-        'promise/prefer-await-to-callbacks': 'error',
         'promise/prefer-await-to-then': 'error',
         // eslint
         'array-bracket-spacing': 'error',
