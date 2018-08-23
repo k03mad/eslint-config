05bfd49f3d8ed4ae22044203439b400ac69a8852
k03mad <k03.mad@gmail.com>
2018-08-23 15:50:48 +0300

    no-continue off

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ module.exports = {
         'no-compare-neg-zero': 'error',
         'no-confusing-arrow': 'error',
         'no-console': 'off',
+        'no-continue': 'off',
         'no-div-regex': 'error',
         'no-duplicate-imports': 'error',
         'no-else-return': 'error',
