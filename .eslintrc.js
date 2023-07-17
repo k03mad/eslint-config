438f7ea58810e9209eee1a8539db92c8346ac361
k03mad <k03.mad@gmail.com>
2023-07-17 14:57:51 +0300

    node-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,18 +52,18 @@ module.exports = {
         'simple-import-sort/imports': 'error',
         'simple-import-sort/exports': 'error',
 
-        // node
-        'node/exports-style': 'error',
-        'node/no-missing-import': 'off',
-        'node/prefer-global/buffer': 'error',
-        'node/prefer-global/console': 'error',
-        'node/prefer-global/process': 'error',
-        'node/prefer-global/text-decoder': 'error',
-        'node/prefer-global/text-encoder': 'error',
-        'node/prefer-global/url-search-params': 'error',
-        'node/prefer-global/url': 'error',
-        'node/prefer-promises/dns': 'error',
-        'node/prefer-promises/fs': 'error',
+        // n
+        'n/exports-style': 'error',
+        'n/prefer-global/buffer': 'error',
+        'n/prefer-global/console': 'error',
+        'n/prefer-global/process': 'error',
+        'n/prefer-global/text-decoder': 'error',
+        'n/prefer-global/text-encoder': 'error',
+        'n/prefer-global/url-search-params': 'error',
+        'n/prefer-global/url': 'error',
+        'n/prefer-promises/dns': 'error',
+        'n/prefer-promises/fs': 'error',
+        'n/no-missing-import': 'off',
 
         // unicorn
         'unicorn/catch-error-name': ['error', {name: 'err'}],
