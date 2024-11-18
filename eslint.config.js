c46634527db8f81fc10060b7cda24dd569a01ccf
k03mad <k03.mad@gmail.com>
2024-11-18 18:18:51 +0300

    update deps

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,9 @@ export default [
 
     {
         languageOptions: {
+            parserOptions: {
+                ecmaVersion: 'latest',
+            },
             globals: {
                 ...globals.browser,
                 ...globals.nodeBuiltin,
