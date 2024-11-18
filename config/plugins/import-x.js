412cd0b3539df52a822273d3d02e2f5206f2a25b
k03mad <k03.mad@gmail.com>
2024-11-18 17:14:34 +0300

    remove double

--- config/plugins/import-x.js
+++ config/plugins/import-x.js
@@ -10,7 +10,6 @@ export default [
             'import-x/extensions': ['error', {ignorePackages: true, pattern: {js: 'always'}}],
             'import-x/first': 'error',
             'import-x/newline-after-import': 'error',
-            'import-x/no-unresolved': ['error', {ignore: ['^p-queue$', '^got$', '^cacheable-lookup$']}],
             'import-x/order': ['error', {'alphabetize': {order: 'asc'}, 'newlines-between': 'always'}],
 
             ...turnOff(
